0f31e1078f9ef028cc986be296927a375302963e
Tylor Steinberger <tlsteinberger167@gmail.com>
2021-06-08 16:29:33 -0400

    fix: allow local environment variables to take precedence

--- src/setEnvVars.ts
+++ src/setEnvVars.ts
@@ -1,9 +1,17 @@
 export function setEnvVars(environmentVariables: Readonly<Record<string, string>>, namespace?: string) {
   for (const [key, value] of Object.entries(environmentVariables)) {
-    process.env[key] = value
+    setEnvVar(key, value)
 
     if (namespace) {
-      process.env[`${namespace}_${key}`] = value
+      setEnvVar(`${namespace}_${key}`, value)
     }
   }
+}
+
+function setEnvVar(key: string, value: string) {
+  if (process.env[key]) {
+    return
+  }
+
+  process.env[key] = value
 }
