6392659a9878c6b8cc353e8f3e5a0c17c3ed6aa0
Tylor Steinberger <tlsteinberger167@gmail.com>
2022-03-08 12:20:05 -0500

    feat: support for serverless 3.x

--- src/createServerless.ts
+++ src/createServerless.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import Serverless from 'serverless'
-import resolveCliInput from 'serverless/lib/cli/resolve-input'
+import resolveInput from 'serverless/lib/cli/resolve-input'
+import resolveVariables from 'serverless/lib/configuration/variables/resolve'
+import resolveVariablesMeta from 'serverless/lib/configuration/variables/resolve-meta'
 
 import { getConfiguration } from './getConfiguration'
 
@@ -11,29 +14,74 @@ export type ServerlessOptions = {
 
 export async function createServerless({ stage = DEFAULT_STAGE }: ServerlessOptions = {}) {
   const { configurationPath, configuration } = await getConfiguration()
-  const { commands, options } = resolveCliInput()
+  const { commands, options, commandSchema } = resolveInput()
   const serverless = new Serverless({
+    configuration,
+    configurationPath,
     commands,
     options: stage ? { ...options, stage } : options,
-    configurationPath,
-    configuration,
     stage,
   })
 
   await serverless.init()
 
-  await setup(serverless)
+  await setup(serverless, configuration, options, commandSchema)
 
-  if (serverless.invokedInstance) {
-    await setup(serverless.invokedInstance)
-  }
-
-  return serverless
+  return [serverless, configuration] as const
 }
 
-async function setup(serverless: Serverless) {
-  await serverless.variables.populateService(serverless.pluginManager.cliOptions)
+async function setup(serverless: any, config: any, options: any, commandSchema: any) {
+  if (serverless.configurationInput) serverless.service.reloadServiceFileParam()
 
+  // Some plugins acccess `options` through `serverless.variables`
+  serverless.variables.options = serverless.pluginManager.cliOptions
+
+  // merge arrays after variables have been populated
+  // (https://github.com/serverless/serverless/issues/3511)
   serverless.service.mergeArrays()
+
+  // populate function names after variables are loaded in case functions were externalized
+  // (https://github.com/serverless/serverless/issues/2997)
   serverless.service.setFunctionNames(serverless.processedInput.options)
+
+  // If in context of service, validate the service configuration
+  if (serverless.serviceDir) await serverless.service.validate()
+
+  serverless.serviceOutputs = new Map()
+  serverless.servicePluginOutputs = new Map()
+
+  const meta = await resolveVariablesMeta(config)
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const providerName = require('serverless/lib/configuration/resolve-provider-name')(config)
+  const resolverConfiguration = {
+    serviceDir: process.cwd(),
+    configuration: config,
+    variablesMeta: meta,
+    sources: {
+      env: require('serverless/lib/configuration/variables/sources/env'),
+      file: require('serverless/lib/configuration/variables/sources/file'),
+      opt: require('serverless/lib/configuration/variables/sources/opt'),
+      self: require('serverless/lib/configuration/variables/sources/self'),
+      strToBool: require('serverless/lib/configuration/variables/sources/str-to-bool'),
+      sls: require('serverless/lib/configuration/variables/sources/instance-dependent/get-sls')(),
+    },
+    options: require('serverless/lib/cli/filter-supported-options')(options, { commandSchema, providerName }),
+    fulfilledSources: new Set(),
+    propertyPathsToResolve: new Set(createPathToResolve(config)),
+    variableSourcesInConfig: new Set(),
+  }
+
+  await resolveVariables(resolverConfiguration)
+}
+
+function createPathToResolve(config: any): string[] {
+  return Object.entries(config).flatMap(([k, v]) =>
+    v != null && !Array.isArray(v) && typeof v === 'object'
+      ? Object.entries(v as object).flatMap(([k2, v2]) => [
+          `${k}\0${k2}`,
+          ...createPathToResolve(v2).map((k3) => `${k}\0${k2}\0${k3}`),
+        ])
+      : [k],
+  )
 }
--- src/getEnvVars.ts
+++ src/getEnvVars.ts
@@ -3,8 +3,13 @@ import { createServerless, ServerlessOptions } from './createServerless'
 /**
  * Retrieve an Record of all the environment variables set by serverless, including any plugins.
  */
-export async function getEnvVars(options: ServerlessOptions = {}): Promise<Record<string, string>> {
-  const serverless = await createServerless(options)
+export async function getEnvVars(opts: ServerlessOptions = {}): Promise<Record<string, string>> {
+  const [serverless, config] = await createServerless(opts)
+  const vars = Object.assign(
+    {},
+    (config.provider as any).environment ?? {},
+    ...Object.values(serverless.service.functions).map((f) => f.environment || {}),
+  )
 
-  return serverless.invokedInstance?.service.provider.environment ?? serverless.service.provider.environment ?? {}
+  return vars
 }
